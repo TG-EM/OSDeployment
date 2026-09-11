70b6206db927b2c3d986a417aa71f44515f13dd2
TG-EM <osdcloud_github@tolsmagrisnich.com>
2026-09-11 10:57:02 +0200

    Update tg-ap-menu.ps1

--- tg-ap-menu.ps1
+++ tg-ap-menu.ps1
@@ -2,24 +2,34 @@
 # Window Functions
 #================================================
 
-$Script:showWindowAsync = Add-Type -MemberDefinition @"
+$script:showWindowAsync = Add-Type -MemberDefinition @"
 [DllImport("user32.dll")]
 public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
 "@ -Name "Win32ShowWindowAsync" -Namespace Win32Functions -PassThru
 
 function Hide-CmdWindow {
     $CMDProcess = Get-Process -Name cmd -ErrorAction Ignore
+
     foreach ($Item in $CMDProcess) {
-        $null = $showWindowAsync::ShowWindowAsync((Get-Process -Id $Item.Id).MainWindowHandle, 2)
+        $null = $script:showWindowAsync::ShowWindowAsync(
+            (Get-Process -Id $Item.Id).MainWindowHandle,
+            2
+        )
     }
 }
 
 function Hide-PowershellWindow {
-    $null = $showWindowAsync::ShowWindowAsync((Get-Process -Id $PID).MainWindowHandle, 2)
+    $null = $script:showWindowAsync::ShowWindowAsync(
+        (Get-Process -Id $PID).MainWindowHandle,
+        2
+    )
 }
 
 function Show-PowershellWindow {
-    $null = $showWindowAsync::ShowWindowAsync((Get-Process -Id $PID).MainWindowHandle, 10)
+    $null = $script:showWindowAsync::ShowWindowAsync(
+        (Get-Process -Id $PID).MainWindowHandle,
+        10
+    )
 }
 
 Hide-CmdWindow
@@ -36,6 +46,10 @@ $Form1 = New-Object System.Windows.Forms.Form
 $Form1.Text = "Step 1 - Select GroupTag"
 $Form1.Size = New-Object System.Drawing.Size(500,400)
 $Form1.StartPosition = "CenterScreen"
+$Form1.TopMost = $true
+$Form1.FormBorderStyle = 'FixedDialog'
+$Form1.MaximizeBox = $false
+$Form1.MinimizeBox = $false
 
 $Label1 = New-Object System.Windows.Forms.Label
 $Label1.Location = New-Object System.Drawing.Point(10,10)
@@ -47,15 +61,20 @@ $List1 = New-Object System.Windows.Forms.ListBox
 $List1.Location = New-Object System.Drawing.Point(10,40)
 $List1.Size = New-Object System.Drawing.Size(300,250)
 
-[void]$List1.Items.Add("PersonalUserNL")
-[void]$List1.Items.Add("PersonalUserEN")
-[void]$List1.Items.Add("PersonalUserDE")
-[void]$List1.Items.Add("PersonalUserFR")
+@(
+    "PersonalUserNL"
+    "PersonalUserEN"
+    "PersonalUserDE"
+    "PersonalUserFR"
+    "PersonalAdminNL"
+    "PersonalAdminEN"
+    "PersonalAdminDE"
+    "PersonalAdminFR"
+) | ForEach-Object {
+    [void]$List1.Items.Add($_)
+}
 
-[void]$List1.Items.Add("PersonalAdminNL")
-[void]$List1.Items.Add("PersonalAdminEN")
-[void]$List1.Items.Add("PersonalAdminDE")
-[void]$List1.Items.Add("PersonalAdminFR")
+$List1.SelectedIndex = 0
 
 $Form1.Controls.Add($List1)
 
@@ -65,19 +84,28 @@ $Btn1.Location = New-Object System.Drawing.Point(180,310)
 $Btn1.DialogResult = [System.Windows.Forms.DialogResult]::OK
 $Form1.Controls.Add($Btn1)
 
+$Cancel1 = New-Object System.Windows.Forms.Button
+$Cancel1.Text = "Cancel"
+$Cancel1.Location = New-Object System.Drawing.Point(280,310)
+$Cancel1.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+$Form1.Controls.Add($Cancel1)
+
 $Form1.AcceptButton = $Btn1
-$Form1.TopMost = $true
+$Form1.CancelButton = $Cancel1
 
 $result1 = $Form1.ShowDialog()
 
 if ($result1 -ne [System.Windows.Forms.DialogResult]::OK) {
-    Exit
+    $Form1.Dispose()
+    throw "Deployment cancelled during GroupTag selection."
 }
 
 $Global:GroupTag = $List1.SelectedItem
 
+$Form1.Dispose()
+
 if (-not $Global:GroupTag) {
-    Exit
+    throw "No GroupTag selected."
 }
 
 # ==========================================
@@ -88,21 +116,31 @@ $Form2 = New-Object System.Windows.Forms.Form
 $Form2.Text = "Step 2 - Select Windows Language"
 $Form2.Size = New-Object System.Drawing.Size(500,350)
 $Form2.StartPosition = "CenterScreen"
+$Form2.TopMost = $true
+$Form2.FormBorderStyle = 'FixedDialog'
+$Form2.MaximizeBox = $false
+$Form2.MinimizeBox = $false
 
 $Label2 = New-Object System.Windows.Forms.Label
 $Label2.Location = New-Object System.Drawing.Point(10,10)
-$Label2.Size = New-Object System.Drawing.Size(350,20)
-$Label2.Text = "Select Windows Installation Language"
+$Label2.Size = New-Object System.Drawing.Size(450,20)
+$Label2.Text = "Select Windows Installation Language for $Global:GroupTag"
 $Form2.Controls.Add($Label2)
 
 $List2 = New-Object System.Windows.Forms.ListBox
 $List2.Location = New-Object System.Drawing.Point(10,40)
 $List2.Size = New-Object System.Drawing.Size(250,180)
 
-[void]$List2.Items.Add("Dutch")
-[void]$List2.Items.Add("English")
-[void]$List2.Items.Add("German")
-[void]$List2.Items.Add("French")
+@(
+    "Dutch"
+    "English"
+    "German"
+    "French"
+) | ForEach-Object {
+    [void]$List2.Items.Add($_)
+}
+
+$List2.SelectedIndex = 0
 
 $Form2.Controls.Add($List2)
 
@@ -112,19 +150,28 @@ $Btn2.Location = New-Object System.Drawing.Point(180,250)
 $Btn2.DialogResult = [System.Windows.Forms.DialogResult]::OK
 $Form2.Controls.Add($Btn2)
 
+$Cancel2 = New-Object System.Windows.Forms.Button
+$Cancel2.Text = "Cancel"
+$Cancel2.Location = New-Object System.Drawing.Point(280,250)
+$Cancel2.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+$Form2.Controls.Add($Cancel2)
+
 $Form2.AcceptButton = $Btn2
-$Form2.TopMost = $true
+$Form2.CancelButton = $Cancel2
 
 $result2 = $Form2.ShowDialog()
 
 if ($result2 -ne [System.Windows.Forms.DialogResult]::OK) {
-    Exit
+    $Form2.Dispose()
+    throw "Deployment cancelled during language selection."
 }
 
 $Language = $List2.SelectedItem
 
+$Form2.Dispose()
+
 if (-not $Language) {
-    Exit
+    throw "No language selected."
 }
 
 # ==========================================
@@ -147,6 +194,10 @@ switch ($Language) {
 
     "French" {
         $Global:OSLanguage = "fr-FR"
+    }
+
+    default {
+        $Global:OSLanguage = "nl-NL"
     }
 }
 
